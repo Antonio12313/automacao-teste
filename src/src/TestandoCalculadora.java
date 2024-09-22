88cf2b73254be113c6d31697ecfec90b7781f3b5
Antonio Reis <antonio.reis@marata.com.br>
2024-09-22 18:41:07 -0300

    ajustando teste

--- src/src/TestandoCalculadora.java
+++ src/src/TestandoCalculadora.java
@@ -1,21 +1,21 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class TestandoCalculadora {
+
     @Test
-    public void SomandoDoisNumeros(){
-        Calculadora calculadora  = new Calculadora();
-        double soma = calculadora.Somar(20,30);
-        System.out.println(soma);
-        Assertions.assertEquals(50,soma);
-    }
-    @Test
-    public void MultiplicandoDoisNumeros(){
+    public void SomandoDoisNumeros() {
         Calculadora calculadora = new Calculadora();
-        double multiplicacao= calculadora.Multiplicar(2.5,10.0);
-        System.out.println(multiplicacao);
-        Assertions.assertEquals(25.0,multiplicacao);
+        double soma = calculadora.Somar(20, 30);
+        System.out.println(soma);
+        assertEquals(50.0, soma, 0.0001);
     }
 
+    @Test
+    public void MultiplicandoDoisNumeros() {
+        Calculadora calculadora = new Calculadora();
+        double multiplicacao = calculadora.Multiplicar(2.5, 10.0);
+        System.out.println(multiplicacao);
+        assertEquals(25.0, multiplicacao, 0.0001);
+    }
 }
