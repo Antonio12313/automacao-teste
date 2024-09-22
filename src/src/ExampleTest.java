58acb0eaf66205f37fd063555ef09d2a7f877846
Antonio Reis <antonio.reis@marata.com.br>
2024-09-22 19:24:17 -0300

    teste sucesso

---
+++ src/src/ExampleTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.assertEquals;
+
+public class ExampleTest {
+    @org.testng.annotations.Test
+    public void testAddition() {
+        assertEquals(2, 1 + 1);
+    }
+}
